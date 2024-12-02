f1245315b72f506638cbfaa750e3fa3ee9fe5850
Dennis Moes <dennismoes@me.com>
2024-12-02 13:58:49 +0100

    refactor: rewrote the disk loading segment
    Moved to ah42 extended read function for the disk loading

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -1,44 +1,98 @@
 [bits 16]
-[org 0x7c00]                ; Shift to bootloader offset
-KERNEL_OFFSET equ 0x1000    ; Memory offset in which we will load our kernel
+[org 0x7C00]
 
-mov [BOOT_DRIVE_ID], dl
+global _start
+_start:
+    cli
 
-mov bp, 0x9000              ; Set up stack at 0x9000
-mov sp, bp
+    ; segment setup
+    mov ax, cs
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
 
-xor ax, ax
-mov ds, ax                  ; Make sure DS is set to 0
+    ; Place stack pointer in the middle of the free memory area
+    mov sp, 0x3000
 
-mov dl, [BOOT_DRIVE_ID]     ; Load the boot drive ID in DL
-mov bx, KERNEL_OFFSET       ; Load 2 sectors to 0x0000(ES):0x1000(BX)
-mov dh, 54                  ; (54 * 512b) = 27.648b + 512b (bootloader)
-call disk_load
+    ; Save drive number to read kernel later with
+    mov [drive_num], dl
 
-mov ah, 0x00                ; int=10/ah=0x0 -> BIOS video mode
-mov al, 0x13                ; 
-int 0x10                    ; Fire in the hole
+    sti
 
-; Enable a20 line
-in al, 0xee
+    ; Print "PONG OS"
+    mov si, welcome_str
+    call print
 
-call switch_to_pm
+    ; Prepare disk_packet
+    mov byte [disk_packet], 0x10
+    mov byte [disk_packet + 1], 0x00
+    mov word [disk_packet + 2], 0x0040
+    mov word [disk_packet + 4], 0x0000
+    mov word [disk_packet + 6], 0x1000
+    mov dword [disk_packet + 8], 1
+    mov dword [disk_packet + 12], 0
 
-jmp $
+    mov si, disk_packet
+    mov ah, 0x42
+    mov dl, [drive_num]
+    int 0x13
+    jc disk_error
 
-%include "src/bootloader/print_function.asm"
-%include "src/bootloader/load_disk.asm"
-%include "src/bootloader/gdt.asm"
-%include "src/bootloader/protected_mode.asm"
+        
+    ; Enable A20 line and disable interrupts
+    cli
 
-[bits 32]
-begin_pm:
-    call KERNEL_OFFSET
-    jmp $
+_loop:
+    jmp _loop
 
-BOOT_DRIVE_ID: db 0
-BEFORE: db 'Before protected mode', 0x0A
-PROTECTED_MODE: db 'We have entered protected mode', 0x0
+; Prints string at DS:SI
+print:
+    xor bh, bh
+    mov ah, 0x0E
+print_loop:
+    lodsb
+    cmp al, 0
+    je print_done
+    int 0x10
+    jmp print_loop
+print_done:
+    ret
 
-times 510-($-$$) db 0       ; Pad the boot sector with zeroes
-dw 0xaa55                   ; Last two bytes tells the BIOS it's a boot sector
+disk_error:
+    mov si, disk_error_str
+    call print
+
+; Variables
+welcome_str    db "PONG OS!!", 0
+disk_error_str db "DISK ERROR", 0
+
+drive_num db 0x00
+disk_packet db 16 dup(0)
+
+
+; GDT
+gdtp:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+gdt_start:
+gdt_null:
+    dq 0
+gdt_code_segment:
+    dw 0xFFFF, 0x0000
+    db 0x00, 0b10011010, 0b11001111, 0x00
+gdt_data_segment:
+    dw 0xFFFF, 0x0000
+    db 0x00, 0b10010010, 0b11001111, 0x00
+gdt_end:
+
+; IDT
+idt:
+    dw 0
+    dd 0
+
+; MBR BOOT SIGNATURE
+times 510-($-$$) db 0
+dw 0xAA55
